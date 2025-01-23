f9d11ed5a4314b3d4b4ee84ad667eadf59768456
Sibeykin <akkbronza4@gmail.com>
2025-01-23 11:49:00 +0300

    Add files via upload

---
+++ dkr14.1.pas
@@ -0,0 +1,32 @@
+﻿program PalindromeCheck;
+
+function P_Alindrom(S: string): Boolean;
+begin
+  if Length(S) <= 1 then
+    P_Alindrom := True
+  else
+  begin
+    if S[1] <> S[Length(S)] then
+      P_Alindrom := False
+    else
+      P_Alindrom := P_Alindrom(Copy(S, 2, Length(S) - 2));
+  end;
+end;
+
+var
+  S1, S2, S3, S4, S5: string;
+begin
+  S1 := 'анна';
+  S2 := 'як';
+  S3 := 'мак';
+  S4 := 'ток';
+  S5 := 'лол';
+
+  WriteLn('P_Alindrom("', S1, '"): ', P_Alindrom(S1));
+  WriteLn('P_Alindrom("', S2, '"): ', P_Alindrom(S2));
+  WriteLn('P_Alindrom("', S3, '"): ', P_Alindrom(S3));
+  WriteLn('P_Alindrom("', S4, '"): ', P_Alindrom(S4));
+  WriteLn('P_Alindrom("', S5, '"): ', P_Alindrom(S5));
+
+  ReadLn;
+end.
---
+++ lb14.4.pas
@@ -0,0 +1,30 @@
+﻿program DistanceCalculation;
+
+type
+  TPoint = record
+    X, Y: Real;
+  end;
+
+function Leng(A, B: TPoint): Real;
+begin
+  Leng := Sqrt(Sqr(A.X - B.X) + Sqr(A.Y - B.Y));
+end;
+
+var
+  A, B, C, D: TPoint;
+  AB, AC, AD: Real;
+
+begin
+  A.X := 1;  A.Y := 1;
+  B.X := 4;  B.Y := 6;
+  C.X := 7;  C.Y := 1;
+  D.X := 3;  D.Y := 5;
+
+  AB := Leng(A, B);
+  AC := Leng(A, C);
+  AD := Leng(A, D);
+
+  Writeln('длина AB: ', AB:0:2);
+  Writeln('длина AC: ', AC:0:2);
+  Writeln('длина AD: ', AD:0:2);
+end.
